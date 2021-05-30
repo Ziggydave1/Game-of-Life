f3668ec8213362feb97485a9b316ad25c7818281
Ziggydave <65269777+Ziggydave@users.noreply.github.com>
2021-05-29 18:30:54 -0600

    Bruh, I misspelled column

--- game_of_life.py
+++ game_of_life.py
@@ -1,22 +1,22 @@
 class Cell:
-    def __init__(self, collumn, row, grid_collumns, grid_rows) -> None:
+    def __init__(self, column, row, grid_columns, grid_rows) -> None:
         self.status = 0
         self.row = row
-        self.collumn = collumn
+        self.column = column
         self.neighbors = []
-        self.find_neighbors(grid_collumns, grid_rows)
+        self.find_neighbors(grid_columns, grid_rows)
         self.live_neighbors = 0
         self.next_state = 0
     
-    def find_neighbors(self, grid_collumns, grid_rows):
+    def find_neighbors(self, grid_columns, grid_rows):
         self.neighbors = []
         for row in range(self.row - 1, self.row + 2):
             if (row >= 0) and (row <= grid_rows - 1):
-                for collumn in range(self.collumn - 1, self.collumn + 2):
-                    if (collumn >= 0) and (collumn <= grid_collumns - 1):
-                        if (row == self.row) and (collumn == self.collumn):
+                for column in range(self.column - 1, self.column + 2):
+                    if (column >= 0) and (column <= grid_columns - 1):
+                        if (row == self.row) and (column == self.column):
                             continue
-                        self.neighbors.append([collumn, row])
+                        self.neighbors.append([column, row])
 
     def find_live_neighbors(self, grid):
         self.live_neighbors = 0
@@ -41,113 +41,113 @@ class Cell:
 
 
 class Grid:
-    def __init__(self, grid_collumns, grid_rows) -> None:
+    def __init__(self, grid_columns, grid_rows) -> None:
         self.grid_rows = grid_rows
-        self.grid_collumns = grid_collumns
+        self.grid_columns = grid_columns
         self.grid = []
-        self.populate(grid_collumns, grid_rows)
+        self.populate(grid_columns, grid_rows)
     
-    def populate(self, grid_collumns, grid_rows):
+    def populate(self, grid_columns, grid_rows):
         for row in range(grid_rows):
             self.grid.append([])
-            for collumn in range(grid_collumns):
-                self.grid[row].append(Cell(collumn, row, self.grid_collumns, self.grid_rows))
+            for column in range(grid_columns):
+                self.grid[row].append(Cell(column, row, self.grid_columns, self.grid_rows))
     
-    def grid_size_change(self, new_grid_collumns, new_grid_rows):
+    def grid_size_change(self, new_grid_columns, new_grid_rows):
         row_change = new_grid_rows - self.grid_rows
-        collumn_change = new_grid_collumns - self.grid_collumns
+        column_change = new_grid_columns - self.grid_columns
         if row_change > 0:
             for new_row in range(row_change):
                 self.grid.append([])
-                for collumn in range(self.grid_collumns):
-                    self.grid[self.grid_rows + new_row].append(Cell(collumn, (self.grid_rows + new_row), self.grid_collumns, self.grid_rows))
-            for collumn in range(self.grid_collumns):
-                self.grid[self.grid_rows - 1][collumn].find_neighbors(new_grid_collumns, new_grid_rows)
-                self.grid[self.grid_rows][collumn].find_live_neighbors(self.grid)
+                for column in range(self.grid_columns):
+                    self.grid[self.grid_rows + new_row].append(Cell(column, (self.grid_rows + new_row), self.grid_columns, self.grid_rows))
+            for column in range(self.grid_columns):
+                self.grid[self.grid_rows - 1][column].find_neighbors(new_grid_columns, new_grid_rows)
+                self.grid[self.grid_rows][column].find_live_neighbors(self.grid)
         elif row_change < 0:
             for deleted_row in range(row_change * -1):
                 self.grid.pop()
-            for collumn in range(self.grid_collumns):
-                self.grid[new_grid_rows - 1][collumn].find_neighbors(new_grid_collumns, new_grid_rows)
-                self.grid[new_grid_rows - 1][collumn].find_live_neighbors(self.grid)
+            for column in range(self.grid_columns):
+                self.grid[new_grid_rows - 1][column].find_neighbors(new_grid_columns, new_grid_rows)
+                self.grid[new_grid_rows - 1][column].find_live_neighbors(self.grid)
 
-        if collumn_change > 0:
+        if column_change > 0:
             for row in range(new_grid_rows):
-                for new_collumn in range(collumn_change):
-                    self.grid[row].append(Cell((self.grid_collumns + new_collumn), row, self.grid_collumns, self.grid_rows))
+                for new_column in range(column_change):
+                    self.grid[row].append(Cell((self.grid_columns + new_column), row, self.grid_columns, self.grid_rows))
             for row in range(new_grid_rows):
-                self.grid[row][self.grid_collumns - 1].find_neighbors(new_grid_collumns, new_grid_rows)
-                self.grid[row][self.grid_collumns].find_live_neighbors(self.grid)
-        elif collumn_change < 0:
+                self.grid[row][self.grid_columns - 1].find_neighbors(new_grid_columns, new_grid_rows)
+                self.grid[row][self.grid_columns].find_live_neighbors(self.grid)
+        elif column_change < 0:
             for row in range(new_grid_rows):
-                for deleted_collumn in range(collumn_change * -1):
+                for deleted_column in range(column_change * -1):
                     self.grid[row].pop()
             for row in range(new_grid_rows):
-                self.grid[row][new_grid_collumns - 1].find_neighbors(new_grid_collumns, new_grid_rows)
-                self.grid[row][new_grid_collumns - 1].find_live_neighbors(self.grid)
+                self.grid[row][new_grid_columns - 1].find_neighbors(new_grid_columns, new_grid_rows)
+                self.grid[row][new_grid_columns - 1].find_live_neighbors(self.grid)
 
-        self.grid_collumns = new_grid_collumns
+        self.grid_columns = new_grid_columns
         self.grid_rows = new_grid_rows
     
-    def make(self, collumn, row, value):
+    def make(self, column, row, value):
         if (value == 1) or (value == 0):
-            if self.grid[row][collumn].status == value:
+            if self.grid[row][column].status == value:
                 return
             else:
-                self.toggle(collumn, row)
+                self.toggle(column, row)
     
-    def safe_make(self, collumn, row, value):
+    def safe_make(self, column, row, value):
         if (value == 1) or (value == 0):
-            if self.grid[row][collumn].status == value:
+            if self.grid[row][column].status == value:
                 return
             else:
-                self.safe_toggle(collumn, row)
+                self.safe_toggle(column, row)
     
-    def toggle(self, collumn, row):
-        if self.grid[row][collumn].status == 1:
-            self.grid[row][collumn].status = 0
-            self.grid[row][collumn].find_next_state()
-            for neighbor in self.grid[row][collumn].neighbors:
+    def toggle(self, column, row):
+        if self.grid[row][column].status == 1:
+            self.grid[row][column].status = 0
+            self.grid[row][column].find_next_state()
+            for neighbor in self.grid[row][column].neighbors:
                 self.grid[neighbor[1]][neighbor[0]].live_neighbors -= 1
                 self.grid[neighbor[1]][neighbor[0]].find_next_state()
         else:
-            self.grid[row][collumn].status = 1
-            self.grid[row][collumn].find_next_state()
-            for neighbor in self.grid[row][collumn].neighbors:
+            self.grid[row][column].status = 1
+            self.grid[row][column].find_next_state()
+            for neighbor in self.grid[row][column].neighbors:
                 self.grid[neighbor[1]][neighbor[0]].live_neighbors += 1
                 self.grid[neighbor[1]][neighbor[0]].find_next_state()
     
-    def safe_toggle(self, collumn, row):
-        if self.grid[row][collumn].status == 1:
-            self.grid[row][collumn].status = 0
-            self.grid[row][collumn].find_next_state()
-            for neighbor in self.grid[row][collumn].neighbors:
+    def safe_toggle(self, column, row):
+        if self.grid[row][column].status == 1:
+            self.grid[row][column].status = 0
+            self.grid[row][column].find_next_state()
+            for neighbor in self.grid[row][column].neighbors:
                 self.grid[neighbor[1]][neighbor[0]].live_neighbors -= 1
         else:
-            self.grid[row][collumn].status = 1
-            self.grid[row][collumn].find_next_state()
-            for neighbor in self.grid[row][collumn].neighbors:
+            self.grid[row][column].status = 1
+            self.grid[row][column].find_next_state()
+            for neighbor in self.grid[row][column].neighbors:
                 self.grid[neighbor[1]][neighbor[0]].live_neighbors += 1
 
     def update(self):
         for row in range(self.grid_rows):
-            for collumn in range(self.grid_collumns):
-                self.safe_make(collumn, row, (self.grid[row][collumn].next_state))
+            for column in range(self.grid_columns):
+                self.safe_make(column, row, (self.grid[row][column].next_state))
         for row in range(self.grid_rows):
-            for collumn in range(self.grid_collumns):
-                self.grid[row][collumn].find_next_state()
+            for column in range(self.grid_columns):
+                self.grid[row][column].find_next_state()
         #Make a queue for cells that changed and their neighbors and only find the next state for them
     
     def clear(self):
         for row in range(self.grid_rows):
-            for collumn in range(self.grid_collumns):
-                self.grid[row][collumn].status = 0
-                self.grid[row][collumn].next_state = 0
-                self.grid[row][collumn].live_neighbors = 0
+            for column in range(self.grid_columns):
+                self.grid[row][column].status = 0
+                self.grid[row][column].next_state = 0
+                self.grid[row][column].live_neighbors = 0
 
-    def get_info(self, collumn, row):
+    def get_info(self, column, row):
         color = (255, 255, 255)
-        status = self.grid[row][collumn].status
+        status = self.grid[row][column].status
         if status == 1:
             color = (255, 165, 0)
         return [status, color]
@@ -155,7 +155,7 @@ class Grid:
     def __repr__(self) -> str:
         string = ''
         for row in range(self.grid_rows):
-            for collumn in range(self.grid_collumns):
-                string += str(self.grid[row][collumn].status)
+            for column in range(self.grid_columns):
+                string += str(self.grid[row][column].status)
             string += '\n'
         return string
--- main.py
+++ main.py
@@ -5,44 +5,44 @@ import time
 
 CELL_SIZE = 14
 ROWS = 50
-COLLUMNS = 50
-WIDTH = CELL_SIZE * COLLUMNS
+COLUMNS = 50
+WIDTH = CELL_SIZE * COLUMNS
 HEIGHT = CELL_SIZE * ROWS
 WINDOW = pygame.display.set_mode(size=(WIDTH, HEIGHT), flags=pygame.RESIZABLE)
 pygame.RESIZABLE
 pygame.display.set_caption('Game Of Life')
 
-board = Grid(COLLUMNS, ROWS)
+board = Grid(COLUMNS, ROWS)
 run = True
 sim_running = False
 
-def draw_grid(win, grid_collumns, grid_rows, size):
+def draw_grid(win, grid_columns, grid_rows, size):
     for row in range(grid_rows):
-        pygame.draw.line(win, (128, 128, 128), (0, row * size), (grid_collumns * size, row * size))
-        for collumn in range(grid_collumns):
-            pygame.draw.line(win, (128, 128, 128), (collumn * size, 0), (collumn * size, grid_rows * size))
+        pygame.draw.line(win, (128, 128, 128), (0, row * size), (grid_columns * size, row * size))
+        for column in range(grid_columns):
+            pygame.draw.line(win, (128, 128, 128), (column * size, 0), (column * size, grid_rows * size))
 
-def draw_cells(win, grid_collumns, grid_rows, size, grid):
+def draw_cells(win, grid_columns, grid_rows, size, grid):
     for row in range(grid_rows):
-        for collumn in range(grid_collumns):
-            pygame.draw.rect(win, grid.get_info(collumn, row)[1], (collumn * size, row * size, size, size))
+        for column in range(grid_columns):
+            pygame.draw.rect(win, grid.get_info(column, row)[1], (column * size, row * size, size, size))
 
-def draw(win, grid_collumns, grid_rows, size, grid):
-    draw_cells(win, grid_collumns, grid_rows, size, grid)
-    draw_grid(win, grid_collumns, grid_rows, size)
+def draw(win, grid_columns, grid_rows, size, grid):
+    draw_cells(win, grid_columns, grid_rows, size, grid)
+    draw_grid(win, grid_columns, grid_rows, size)
     pygame.display.update()
 
 def get_clicked_pos(pos, size):
     x_pos, y_pos = pos
     row = y_pos // size
-    collumn = x_pos // size
+    column = x_pos // size
 
-    return row, collumn
+    return row, column
 
-def simulation(win, grid_rows, grid_collumns, size, grid):
+def simulation(win, grid_rows, grid_columns, size, grid):
     while True:
         grid.update()
-        draw(win, grid_rows, grid_collumns, size, grid)
+        draw(win, grid_rows, grid_columns, size, grid)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
@@ -56,21 +56,21 @@ def simulation(win, grid_rows, grid_collumns, size, grid):
                 return
 
 while run:
-    draw(WINDOW, ROWS, COLLUMNS, CELL_SIZE, board)
+    draw(WINDOW, ROWS, COLUMNS, CELL_SIZE, board)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
-            row, collumn = get_clicked_pos(pos, CELL_SIZE)
-            board.make(collumn, row, 1)
+            row, column = get_clicked_pos(pos, CELL_SIZE)
+            board.make(column, row, 1)
         elif pygame.mouse.get_pressed()[2]:
             pos = pygame.mouse.get_pos()
-            row, collumn = get_clicked_pos(pos, CELL_SIZE)
-            board.make(collumn, row, 0)
+            row, column = get_clicked_pos(pos, CELL_SIZE)
+            board.make(column, row, 0)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                simulation(WINDOW, ROWS, COLLUMNS, CELL_SIZE, board)
+                simulation(WINDOW, ROWS, COLUMNS, CELL_SIZE, board)
             elif event.key == pygame.K_c:
                 board.clear()
